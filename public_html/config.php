c13e5637efe3f7a463eca0cc6f2a776dec6dfd7b
Leon200211 <leon200207@yandex.ru>
2023-09-19 21:37:03 +0300

    Initial

---
+++ public_html/config.php
@@ -0,0 +1,21 @@
+<?php
+
+
+defined('VG_ACCESS') or die('Access denied');
+
+
+// константы
+const SITE_URL = 'http://digital-breakthrough-rutube/';
+
+
+const PATH = '/';
+
+const HOST = 'localhost';
+const USER = 'root';
+const PASS = 'root';
+const DB_NAME = 'digital-breakthrough-rutube';
+
+
+
+
+
